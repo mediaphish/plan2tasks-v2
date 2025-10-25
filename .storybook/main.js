7e3d0ef2676d9cfa23e42c3081b0109772eb502e
mediaphish <mediaphish@users.noreply.github.com>
2025-10-25 13:35:20 -0500

    Create main.js

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
