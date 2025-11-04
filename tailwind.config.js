a1c2e428835c3b285cfcfdfe107ce87ff22ce567
mediaphish <mediaphish@users.noreply.github.com>
2025-11-04 10:43:26 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: {
+          50: '#FAFAF9',
+          100: '#F5F3F0',
+        },
+        charcoal: {
+          600: '#374151',
+          700: '#2D3748',
+          800: '#1F2937',
+        },
+        bronze: {
+          600: '#8B6F47',
+          700: '#6B5435',
+        },
+      },
+    },
   },
   plugins: [],
 }
