31e98567b1be1797f41c863de0a3a114ace9351e
mediaphish <mediaphish@users.noreply.github.com>
2025-11-11 11:15:28 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.jsx', '.js', '.ts', '.tsx', '.mjs', '.json']
+  }
 })
+
